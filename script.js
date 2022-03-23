14957578e3e47c57d926137cb1c10ba874ef56ba
Rachel LeBoeuf <rachelnleboeuf@gmail.com>
2022-03-22 21:50:24 -0400

    added APIs and functions for homepage

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
+// Search Bar and Button
 var searchBtn = document.getElementById('searchBtn');
+const weekdayWeatherDisplay = document.querySelector('weekdayWeatherDisplay');
 console.log(searchBtn);
 const currentWeather = (cityName) => {
     console.log(cityName)
-    var getCurrentWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=01491020703a0729a1904f9d2453e6f1'
+    // CAN I DRY THIS UP BY ASSIGNING VARIABLE NAMES TO API ID? CAN THIS HAVE HTTPS?
+    const getCurrentWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=01491020703a0729a1904f9d2453e6f1'
     console.log(getCurrentWeatherAPI);
+
     fetch(getCurrentWeatherAPI)
         .then(function (response) {
-          //  console.log(response);
-          return response.json()
-        }).then(function(data){
-console.log(data)
+            //  console.log(response);
+            return response.json()
+        }).then(function (data) {
+            console.log(data)
         })
+
+    searchBtn.addEventListener('click', function () {
+        var input = document.getElementById('locationSearch')
+        currentWeather(input.value);
+    })
+
+
+    // CURRENT LOCATION WEATHER DISPLAY VARIABLES AND API
+
+
+    let lon;
+    let lat;
+    let temperature = document.querySelector('.temperature');
+    let sum = document.querySelector('.sum');
+    let location = document.querySelector('.location');
+    let icon = document.querySelector('.icon');
+    const kelvin = 273;
 }
+// CURRENT LOCATION WEATHER ICON ON HOMEPAGE. USE LOAD METHOD TO PUT RETURNED DATA INTO WINDOW. 
+// .coods- .coords returns value of coords attribute. attribute specifies the x & y coordinates of an area
 
 
-searchBtn.addEventListener('click', function () {
-    var input = document.getElementById('locationSearch')
-    currentWeather(input.value);
-})
+window.addEventListener('load', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.lon;
+            lat = position.coords.lat;
+
+            const currentLocationWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+                `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+
+            const idAPI = '&appid=01491020703a0729a1904f9d2453e6f1';
+
+
+
+            // CALLING API
+
+            fetch(currentLocationWeatherAPI)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    '.temperature'.textContent =
+                        Math.floor(data.main.temperature - kelvin) + '°C';
+                    sum.textContent = data.weather[0].description;
+                    location.textContent = data.name + "," + data.sys.country;
+                    let icon1 = data.weather[0].icon;
+                    icon.innerHTML =
+                        `<img src="icon/${icon1}.svg" style= 'height:10rem'/>`;
+                });
+        });
+    }
+});
+
+
