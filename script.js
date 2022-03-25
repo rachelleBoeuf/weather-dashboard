46b4ab9d13900b3a0eb161aee7c6692a260a0a9e
Rachel LeBoeuf <rachelnleboeuf@gmail.com>
2022-03-25 17:25:10 -0400

    dry up code and added additional APIs

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Search Bar and Button
 var searchBtn = document.getElementById('searchBtn');
+let temperature = document.querySelector('.temperature');
+let sum = document.querySelector('.sum');
+let loc = document.querySelector('.location');
+let icon = document.querySelector('.icon');
+let windSpeed = document.querySelector('.wind-speed');
+let humidity = document.querySelector('.humidity');
+let UVI = document.querySelector('.UVI');
+
 const weekdayWeatherDisplay = document.querySelector('weekdayWeatherDisplay');
 console.log(searchBtn);
 const currentWeather = (cityName) => {
@@ -14,25 +22,28 @@ const currentWeather = (cityName) => {
             return response.json()
         }).then(function (data) {
             console.log(data)
+            lat = data.coord.lat;
+            lon = data.coord.lon;
+            console.log('cityName');
+            getFullForecast();
         })
 
-    searchBtn.addEventListener('click', function () {
-        var input = document.getElementById('locationSearch')
-        currentWeather(input.value);
-    })
 
-
-    // CURRENT LOCATION WEATHER DISPLAY VARIABLES AND API
-
-
-    let lon;
-    let lat;
-    let temperature = document.querySelector('.temperature');
-    let sum = document.querySelector('.sum');
-    let location = document.querySelector('.location');
-    let icon = document.querySelector('.icon');
-    const kelvin = 273;
 }
+
+searchBtn.addEventListener('click', function () {
+    var input = document.getElementById('locationSearch')
+    currentWeather(input.value);
+});
+
+// CURRENT LOCATION WEATHER DISPLAY VARIABLES AND API
+
+
+let lon;
+let lat;
+
+
+
 // CURRENT LOCATION WEATHER ICON ON HOMEPAGE. USE LOAD METHOD TO PUT RETURNED DATA INTO WINDOW. 
 // .coods- .coords returns value of coords attribute. attribute specifies the x & y coordinates of an area
 
@@ -41,34 +52,39 @@ window.addEventListener('load', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
             console.log(position);
-            lon = position.coords.lon;
-            lat = position.coords.lat;
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
 
-            const currentLocationWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-                `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+            getFullForecast();
 
-            const idAPI = '&appid=01491020703a0729a1904f9d2453e6f1';
 
 
 
             // CALLING API
 
-            fetch(currentLocationWeatherAPI + idAPI)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    console.log(temperature);
-                    '.temperature'.textContent =
-                        Math.floor(data.main.temperature - kelvin) + '°C';
-                    sum.textContent = data.weather[0].description;
-                    location.textContent = data.name + "," + data.sys.country;
-                    let icon1 = data.weather[0].icon;
-                    icon.innerHTML =
-                        `<img src="icon/${icon1}.svg" style= 'height:10rem'/>`;
-                });
+
         });
     }
 });
 
+const getFullForecast = () => {
+    const currentLocationWeatherAPI = `http://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${lat}&` +
+        `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
 
+    fetch(currentLocationWeatherAPI)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            temperature.textContent = data.current.temp + '°F';
+            sum.textContent = data.current.weather[0].description;
+            // loc.textContent = data. + "," + data.sys.country;
+            windSpeed.textContent = data.current.wind_speed + 'mph';
+            humidity.textContent = data.current.humidity + '%';
+            UVI.textContent = data.current.uvi;
+            let icon1 = data.current.weather[0].icon;
+            icon.innerHTML =
+                `<img src="http://openweathermap.org/img/wn/${icon1}@2x.png" />`;
+        });
+}
